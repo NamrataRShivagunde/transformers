64817248efe3623991f1782468d9a48d9c4bbde7
Nammu <namratashivagunde@gmail.com>
2022-12-12 21:44:15 -0500

    use bnb to multiply

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -21,6 +21,7 @@ import torch
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+import bitsandbytes as bnb
 
 from ...activations import ACT2FN
 from ...modeling_outputs import (
@@ -148,7 +149,7 @@ class OPTNormOutput(nn.Module): # This class is added by Namrata Shivagunde
             
             # Make transformed vectors f(x) from Value vectors (value_layer) and weight matrix (dense).
             # transformed_layer = value_layer.matmul(dense)
-            transformed_layer = torch.bmm(value_layer,dense).unsqueeze(0) # [1, num_heads, s, all_head_size]
+            transformed_layer = bnb.matmul(value_layer,dense).unsqueeze(0) # [1, num_heads, s, all_head_size]
             transformed_norm = torch.norm(transformed_layer, dim=-1) # [1, num_heads, s]
 
             # transformed_shape = transformed_layer.size() #(batch, seq_length, num_heads, 1, all_head_size)
