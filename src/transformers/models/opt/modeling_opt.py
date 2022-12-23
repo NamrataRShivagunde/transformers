c4830e3b6290645d980c40ff2c0e844298146372
Nammu <namratashivagunde@gmail.com>
2022-12-23 11:47:12 -0500

    attention prob unsqueeze

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -156,6 +156,7 @@ class OPTNormOutput(nn.Module): # This class is added by Namrata Shivagunde
             transformed_norm = transformed_norm.unsqueeze(0) # [1, num_heads, s]
 
             transformed_layer = transformed_layer.unsqueeze(0) # [1, num_heads, s, all_head_size]
+            attention_probs = attention_probs.unsqueeze(0) # [1, num_heads, s, s]
 
             # transformed_shape = transformed_layer.size() #(batch, seq_length, num_heads, 1, all_head_size)
             # transformed_layer = transformed_layer.view(transformed_shape[:-2] + (transformed_shape[-1],))
