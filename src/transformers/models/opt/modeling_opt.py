51cbe2c458ed4783debd587e1b6e831e5711c83d
Nammu <namratashivagunde@gmail.com>
2022-12-23 10:03:12 -0500

    dtype print

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -152,7 +152,7 @@ class OPTNormOutput(nn.Module): # This class is added by Namrata Shivagunde
             print(value_layer.shape)
             print(dense.shape)
             print(value_layer.dtype)
-            print(dense.dtypes)
+            print(dense.dtype)
             transformed_layer = torch.bmm(value_layer, dense) # [num_heads, s, all_head_size]
             transformed_norm = torch.norm(transformed_layer, dim=-1) # [num_heads, s]
             transformed_norm = transformed_norm.unsqueeze(0) # [1, num_heads, s]
